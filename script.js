4da6a2ef40bc158e9c6b68bf7a9129791dcd865e
Pedro Bonifacio <pedroboni.dev@gmail.com>
2024-09-02 15:56:17 -0300

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+function sortearTimes() {
+  const numero = parseInt(document.getElementById('numero').value);
+  const resultadoDiv = document.getElementById('resultado');
+
+  if (isNaN(numero) || numero < 1) {
+      resultadoDiv.innerHTML = "<p>Por favor, digite um número válido maior que 0.</p>";
+      return;
+  }
+
+  // Gera números de 1 a n e os embaralha
+  const jogadores = Array.from({length: numero}, (_, i) => i + 1).sort(() => Math.random() - 0.5);
+  const times = [];
+
+  // Divide em times de 5
+  while (jogadores.length) {
+      times.push(jogadores.splice(0, 5));
+  }
+
+  // Exibe os times
+  resultadoDiv.innerHTML = '';
+  times.forEach((time, index) => {
+      resultadoDiv.innerHTML += `<p>Time ${index + 1}: ${time.join(', ')}</p>`;
+  });
+}
